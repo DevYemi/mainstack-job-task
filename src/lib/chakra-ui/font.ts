ea114fe88fc2c91f7ebba4351c93a4cd17af7510
Adeyanju Adeyemi <alex.olaolu007@gmail.com>
2024-04-24 19:43:14 +0100

    chore: renamed font filenames

--- src/lib/chakra-ui/font.ts
+++ src/lib/chakra-ui/font.ts
@@ -3,83 +3,83 @@ import localFont from "next/font/local";
 const degularFont = localFont({
   src: [
     {
-      path: "./../../../public/fonts/degular/Degular-Thin.otf",
+      path: "./../../../public/fonts/degular/degularthin.otf",
       weight: "100",
       style: "normal",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-Light.otf",
+      path: "./../../../public/fonts/degular/degularlight.otf",
       weight: "200",
       style: "normal",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-Medium.otf",
+      path: "./../../../public/fonts/degular/degularmedium.otf",
       weight: "300",
       style: "normal",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-Regular.otf",
+      path: "./../../../public/fonts/degular/degularregular.otf",
       weight: "400",
       style: "normal",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-Medium.otf",
+      path: "./../../../public/fonts/degular/degularmedium.otf",
       weight: "500",
       style: "normal",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-Semibold.otf",
+      path: "./../../../public/fonts/degular/degularsemibold.otf",
       weight: "600",
       style: "normal",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-Bold.otf",
+      path: "./../../../public/fonts/degular/degularbold.otf",
       weight: "700",
       style: "normal",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-Black.otf",
+      path: "./../../../public/fonts/degular/degularblack.otf",
       weight: "800",
       style: "normal",
     },
     // Italics
     {
-      path: "./../../../public/fonts/degular/Degular-ThinItalic.otf",
+      path: "./../../../public/fonts/degular/degularthinitalic.otf",
       weight: "100",
       style: "italic",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-LightItalic.otf",
+      path: "./../../../public/fonts/degular/degularlightitalic.otf",
       weight: "200",
       style: "italic",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-MediumItalic.otf",
+      path: "./../../../public/fonts/degular/degularmediumitalic.otf",
       weight: "300",
       style: "italic",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-RegularItalic.otf",
+      path: "./../../../public/fonts/degular/degularregularitalic.otf",
       weight: "400",
       style: "italic",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-MediumItalic.otf",
+      path: "./../../../public/fonts/degular/degularmediumitalic.otf",
       weight: "500",
       style: "italic",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-SemiboldItalic.otf",
+      path: "./../../../public/fonts/degular/degularsemibolditalic.otf",
       weight: "600",
       style: "italic",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-BoldItalic.otf",
+      path: "./../../../public/fonts/degular/degularbolditalic.otf",
       weight: "700",
       style: "italic",
     },
     {
-      path: "./../../../public/fonts/degular/Degular-BlackItalic.otf",
+      path: "./../../../public/fonts/degular/degularblackitalic.otf",
       weight: "800",
       style: "italic",
     },
